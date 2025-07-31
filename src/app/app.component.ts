1d8a2d8f77cefa1a4ed4b01464c41459d5ca6637
SAMUEL DE JESUS OLIVEIRA <025.026808@universidadesenaicimatec.edu.br>
2025-07-31 16:19:53 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TranslateModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'translate';
+
+  constructor(private translate: TranslateService) {
+    translate.addLangs(['en', 'pt']);
+    translate.setFallbackLang('pt');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en|pt/) ? browserLang : 'pt');
+  }
+
+  useLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from "@angular/core";
+import {provideHttpClient} from "@angular/common/http";
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient} from '@angular/common/http';
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: 'json',
+    }),
+
+    importProvidersFrom([
+      TranslateModule.forRoot()
+    ])
+  ]
 };
