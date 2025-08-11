bf419a3fe38727a8a05d2829ee1ccf497dfb8adf
Samueelljr <samueldejesus705@gmail.com>
2025-08-11 11:45:03 -0300

    Componente Modal de Login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { ModalLoginComponent } from '../modal/modal-login/modal-login.component';
 
 @Component({
   selector: 'app-header',
   imports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    ModalLoginComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
@@ -17,5 +19,20 @@ export class HeaderComponent {
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
+  }
+
+  @ViewChild(ModalLoginComponent, {static: false}) modal!: ModalLoginComponent;
+
+  ngAfterViewInit() {
+    // Só para conferir que o ViewChild foi inicializado
+    console.log('Modal:', this.modal);
+  }
+
+  openLogin() {
+    if (this.modal) {
+      this.modal.open();
+    } else {
+      console.warn('Modal ainda não inicializado');
+    }
   }
 }
